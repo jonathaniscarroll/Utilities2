3d097eaa07aedf9555cb86daa332fa3b82236956
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2023-02-03 16:09:27 -0400

    updated lookattarget, transformmovetoward

--- LookAtTarget.cs
+++ LookAtTarget.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LookAtTarget : MonoBehaviour
 {
+	public Vector3 Mod = Vector3.one;
 	public Transform Target{
 		get;set;
 	}
@@ -21,7 +22,12 @@ public class LookAtTarget : MonoBehaviour
 		if(Target!=null){
 			//Debug.Log(Target.name);
 			Vector3 dir = transform.position - Target.position;
-			//dir.y = 0; // keep the direction strictly horizontal
+			dir.x*=Mod.x;
+			dir.y*=Mod.y;
+			dir.z*=Mod.z;
+			//dir.x = 0; // keep the direction strictly horizontal
+			//dir.z = 0;
+			//dir.y = 0;
 			Quaternion rot = Quaternion.LookRotation(dir);
 			// slerp to the desired rotation over time
 			transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime);
